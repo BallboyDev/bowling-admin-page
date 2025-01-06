d86a47e8f3b7d968bfd06ee094c65cef531c11ce
ballboyDev <yswgood0329@gmail.com>
2025-01-06 16:34:59 +0900

    horizontal scroll

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,25 +6,22 @@
 <h1 class="month">1월</h1>
 <hr />
 
-<div class="wrapper">
-    <div class="names">
-        {#each member as name}
-            <div class="data-item">{name}</div>
-        {/each}
-    </div>
-
-    <table
-        class="scores"
-        width="100%"
-        border="0"
-        cellspacing="0"
-        cellpadding="0"
-    >
+<div class="table-container">
+    <table>
+        <thead>
+            <tr>
+                <th>test</th>
+                {#each days as day, i}
+                    <th>{i + 1}</th>
+                {/each}
+            </tr>
+        </thead>
         <tbody>
             {#each member as name}
                 <tr>
+                    <td>{name}</td>
                     {#each days as day, i}
-                        <td class="data-item">{i + 1}</td>
+                        <td>{i + 1}</td>
                     {/each}
                 </tr>
             {/each}
@@ -32,33 +29,41 @@
     </table>
 </div>
 
-<style lang="scss">
-    .month {
-        padding: 1rem;
+<style>
+    .table-container {
+        width: 100%; /* 전체 테이블 너비 */
+        overflow-x: auto; /* 가로 스크롤 활성화 */
     }
 
-    .wrapper {
-        width: 100%;
-        display: flex;
-
-        & > .names {
-            width: 5rem;
-            & > div {
-                width: 5rem;
-            }
-        }
-
-        & > .scores {
-            width: 100%;
-
-            & > tr {
-                overflow: scroll;
-            }
-        }
+    table {
+        width: 100%; /* 테이블의 기본 너비 */
+        border-collapse: collapse; /* 테두리 중첩 제거 */
     }
 
-    .data-item {
-        width: 3rem;
-        height: 3rem;
+    thead th {
+        position: sticky;
+        top: 0; /* 헤더를 고정 */
+        background-color: #f1f1f1;
+        z-index: 1;
+    }
+
+    th,
+    td {
+        padding: 10px;
+        text-align: left;
+        border: 1px solid #ddd; /* 셀 구분선 */
+        white-space: nowrap; /* 텍스트 줄바꿈 방지 */
+    }
+
+    tbody {
+        display: block; /* tbody를 블록 요소로 설정 */
+        overflow-x: auto; /* 가로 스크롤 활성화 */
+        white-space: nowrap; /* 셀 내용 줄바꿈 방지 */
+    }
+
+    tr {
+        display: table; /* 행을 테이블 요소로 유지 */
+        width: 100%; /* tbody 영역 내에서 테이블 크기를 맞춤 */
+        table-layout: fixed; /* 고정된 테이블 레이아웃 */
     }
 </style>
