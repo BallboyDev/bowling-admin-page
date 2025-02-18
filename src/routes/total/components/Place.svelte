46108cf00af5dd1b620bda4f369445bbb8370462
ballboyDev <yswgood0329@gmail.com>
2025-02-18 17:13:03 +0900

    test

--- src/routes/total/components/Place.svelte
+++ src/routes/total/components/Place.svelte
@@ -3,22 +3,82 @@
     import { Button } from "@sveltestrap/sveltestrap";
     import { Chart, registerables } from "chart.js";
     import { onMount } from "svelte";
+    import { flip } from "svelte/animate";
+    import { blur, fade, slide } from "svelte/transition";
 
     // props, state value
+    let visible = $state(false);
+
+    let list = $state([1, 2, 3, 4]);
 
     onMount(() => {});
 
-    const handle = {};
+    $effect(() => {
+        if (!!visible) {
+            handle.chart();
+        }
+    });
+
+    const handle = {
+        chart: () => {
+            let ctx = document.getElementById("placeChart");
+            new Chart(ctx, {
+                type: "doughnut",
+                data: {
+                    datasets: [
+                        {
+                            data: [180, 120],
+                            backgroundColor: [
+                                "rgb(54, 162, 235)",
+                                "rgb(255, 99, 132, 0)",
+                            ],
+                        },
+                    ],
+                },
+                options: {
+                    plugins: {
+                        tooltip: false,
+                    },
+                },
+                plugins: [
+                    {
+                        beforeDraw: (chart) => {
+                            const ctx = chart.ctx;
+
+                            ctx.font = "50px Arial";
+                            ctx.fillStyle = "#cccccc";
+                            ctx.textAlign = "center";
+                            ctx.fillText(
+                                "180",
+                                ctx.canvas.clientWidth / 2,
+                                ctx.canvas.clientHeight / 2 + 8,
+                            );
+                        },
+                    },
+                ],
+            });
+        },
+    };
     const callApi = {};
 </script>
 
 <div id="places" class="p-2 mb-3 rounded-4">
     <div class="small fw-bold">광교진락볼링장</div>
     <div class="chart d-flex justify-content-center">
-        <canvas id="placeChart" class="w-75 h-75"></canvas>
+        {#if visible}
+            <div class="wrapper" in:fade={{ duration: 1000 }}>
+                <canvas id="placeChart" class="w-100 h-100"></canvas>
+            </div>
+        {/if}
     </div>
-    <!-- <div class="d-flex justify-content-center">* * *</div> -->
+    <div class="d-flex justify-content-center">* * *</div>
     <Button
+        children="check!!!"
+        onclick={() => {
+            visible = !visible;
+        }}
+    />
+    <!-- <Button
         children="BUTTON"
         onclick={() => {
             let ctx = document.getElementById("placeChart");
@@ -58,12 +118,24 @@
                 ],
             });
         }}
-    />
+    /> -->
 </div>
 
 <style lang="scss">
     #places {
         background-color: #282828;
         border: 1px solid gray;
+
+        & > .chart {
+            & > .wrapper {
+                width: 10rem;
+                height: 10rem;
+                border: 1px solid white;
+
+                & > div {
+                    background-color: gray;
+                }
+            }
+        }
     }
 </style>
